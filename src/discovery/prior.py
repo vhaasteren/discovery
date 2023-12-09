bc303ae939636aa30103cf259d373784edb5e75c
Patrick Meyers <pmeyers@caltech.edu>
2023-12-08 17:54:35 -0800

    fix bug in string matching in prior

--- src/discovery/prior.py
+++ src/discovery/prior.py
@@ -4,27 +4,29 @@ import numpy as np
 
 from . import matrix
 
+
 def uniform(par, a, b):
     def logpriorfunc(params):
-        return matrix.jnp.where(matrix.jnp.logical_and(params[par] >= a, params[par] <= b),
-                                0, -matrix.jnp.inf)
+        return matrix.jnp.where(matrix.jnp.logical_and(params[par] >= a, params[par] <= b), 0, -matrix.jnp.inf)
 
     return logpriorfunc
 
+
 priordict_standard = {
-    '.*_rednoise_log10_A': [-18, -11],
-    '.*_rednoise_gamma': [0, 7],
-    '.*_red_noise_log10_A': [-18, -11],  # deprecated
-    '.*_red_noise_gamma': [0, 7],        # deprecated
-    'crn_log10_A': [-18, -11],
-    'crn_gamma': [0, 7],
-    'gw_(.*_)?log10_A': [-18, -11],
-    'gw_(.*_)?gamma': [0, 7],
-    'dmgp_log10_A': [-20, -11],
-    'dmgp_gamma': [0, 7],
-    'crn_log10_rho': [-9, -4],
-    'gw_(.*_)?log10_rho': [-9, -4]
+    ".*_rednoise_log10_A": [-18, -11],
+    ".*_rednoise_gamma": [0, 7],
+    ".*_red_noise_log10_A": [-18, -11],  # deprecated
+    ".*_red_noise_gamma": [0, 7],  # deprecated
+    "crn_log10_A": [-18, -11],
+    "crn_gamma": [0, 7],
+    "gw_(.*_)?log10_A": [-18, -11],
+    "gw_(.*_)?gamma": [0, 7],
+    "dmgp_log10_A": [-20, -11],
+    "dmgp_gamma": [0, 7],
+    "crn_log10_rho": [-9, -4],
+    "gw_(.*_)?log10_rho": [-9, -4],
 }
+
 
 def makelogprior_uniform(params, priordict={}):
     priordict = {**priordict_standard, **priordict}
@@ -32,14 +34,18 @@ def makelogprior_uniform(params, priordict={}):
     priors = []
     for par in params:
         for parname, range in priordict.items():
-            if parname in par:
+            print(parname, par, re.match(parname, par))
+            if re.match(parname, par):
                 priors.append(uniform(par, *range))
                 break
 
     def logprior(params):
+        print(params)
+        print([prior(params) for prior in priors])
         return sum(prior(params) for prior in priors)
 
     return logprior
+
 
 def sample_uniform(params, priordict={}, n=1):
     priordict = {**priordict_standard, **priordict}
@@ -47,13 +53,15 @@ def sample_uniform(params, priordict={}, n=1):
     sample = {}
     for par in params:
         for parname, range in priordict.items():
-            if re.match(parname, par) or parname == par:
-                if par.endswith(')'):
-                    sample[par] = (np.random.uniform(*range, size=int(par[par.index('(')+1:-1])) if n == 1
-                                   else np.random.uniform(*range, size=(n,int(par[par.index('(')+1:-1]))))
+            if re.match(parname, par):
+                if par.endswith(")"):
+                    sample[par] = (
+                        np.random.uniform(*range, size=int(par[par.index("(") + 1 : -1]))
+                        if n == 1
+                        else np.random.uniform(*range, size=(n, int(par[par.index("(") + 1 : -1])))
+                    )
                 else:
-                    sample[par] = (np.random.uniform(*range) if n == 1
-                                   else np.random.uniform(*range, size=n))
+                    sample[par] = np.random.uniform(*range) if n == 1 else np.random.uniform(*range, size=n)
                 break
         else:
             raise KeyError(f"No known prior for {par}.")
