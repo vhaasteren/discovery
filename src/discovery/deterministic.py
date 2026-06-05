Correct chrom exponential delay calculation in deterministic.py
Aditya reported that NaNs resulted from calculating the exponential and then multiplying by heaviside function. Use "where" instead to force 0 for t<t0

src/discovery/deterministic.py
@@ -334,7 +334,10 @@ def chromatic_exponential(psr, fref=1400.0):
         delay : ndarray
             Array of timing residuals :math:`\Delta(t)` in seconds with shape matching psr.toas
         """
-        return jnp.sign(sign_param) * 10**log10_Amp * jnp.exp(- (toas - t0) / 10**log10_tau) * fnorm**alpha * jnp.heaviside(toas - t0, 1.0)
+        dt = toas - t0
+        tau = 10**log10_tau
+        amp = 10**log10_Amp
+        return jnp.sign(sign_param) * amp * fnorm**alpha * jnp.where(dt >= 0, jnp.exp(-dt / tau), 0.0 )
 
     delay.__name__ = "chromatic_exponential_delay"
     return delay